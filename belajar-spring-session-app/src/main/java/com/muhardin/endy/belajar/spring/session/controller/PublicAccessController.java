21462c74a6d8f7a82813f975d59a9f3f415dccb3
Endy Muhardin <endy.muhardin@gmail.com>
2021-04-12 22:52:17 +0700

    remove unused imports

--- belajar-spring-session-app/src/main/java/com/muhardin/endy/belajar/spring/session/controller/PublicAccessController.java
+++ belajar-spring-session-app/src/main/java/com/muhardin/endy/belajar/spring/session/controller/PublicAccessController.java
@@ -2,9 +2,7 @@ package com.muhardin.endy.belajar.spring.session.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
