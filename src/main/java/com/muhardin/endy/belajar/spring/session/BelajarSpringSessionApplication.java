33c73b57e992a6c1606578a8203d6e842cccdb56
Endy Muhardin <endy.muhardin@gmail.com>
2021-04-12 17:27:08 +0700

    starter project

---
+++ src/main/java/com/muhardin/endy/belajar/spring/session/BelajarSpringSessionApplication.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.belajar.spring.session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringSessionApplication.class, args);
+	}
+
+}
